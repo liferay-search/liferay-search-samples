9296fcc8da2a15470d28e6e654204e08804284a3
Russell Bohl <russell.bohl@liferay.com>
2021-08-12 15:10:38 -0400

    LRDOCS-9179 copy in liferay-learn POC workspace with gogo command; rename

---
+++ dev/queries-and-filters/queries-and-filters-osgi-command/src/main/java/com/acme/r2d2/search/osgi/R2D2SearchOSGiCommand.java
@@ -0,0 +1,77 @@
+package com.acme.r2d2.search.osgi;
+
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.search.query.BooleanQuery;
+import com.liferay.portal.search.query.MatchQuery;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.query.TermsQuery;
+import com.liferay.portal.search.searcher.SearchRequest;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+	property = {"osgi.command.function=search", "osgi.command.scope=r2d2"},
+	service = R2D2SearchOSGiCommand.class
+)
+public class R2D2SearchOSGiCommand {
+
+	public void search(String keywords) {
+		MatchQuery titleQuery = _queries.match(
+			Field.getLocalizedName(LocaleUtil.US, Field.TITLE), keywords);
+
+		TermsQuery rootFolderQuery = _queries.terms(Field.ENTRY_CLASS_NAME);
+
+		rootFolderQuery.addValues("com.liferay.journal.model.JournalArticle");
+
+		BooleanQuery booleanQuery = _queries.booleanQuery();
+
+		booleanQuery.addMustQueryClauses(rootFolderQuery, titleQuery);
+
+		SearchRequestBuilder searchRequestBuilder =
+			_searchRequestBuilderFactory.builder();
+
+		long companyId = _portal.getDefaultCompanyId();
+
+		searchRequestBuilder.explain(
+			true
+		).includeResponseString(
+			true
+		).withSearchContext(
+			searchContext -> {
+				searchContext.setCompanyId(companyId);
+				searchContext.setKeywords(keywords);
+			}
+		);
+
+		SearchRequest searchRequest = searchRequestBuilder.query(
+			booleanQuery
+		).build();
+
+		SearchResponse searchResponse = _searcher.search(searchRequest);
+
+		System.out.println(
+			"Request String:\n" + searchResponse.getRequestString());
+		System.out.println(
+			"Response String:\n" + searchResponse.getResponseString());
+	}
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private Queries _queries;
+
+	@Reference
+	private Searcher _searcher;
+
+	@Reference
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+}
