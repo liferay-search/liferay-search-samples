308ce5039d4212afc00e2094677cff6941356fed
Russell Bohl <russell.bohl@liferay.com>
2021-08-26 13:59:33 -0400

    LRDOCS-7800 Process the hits

--- dev/queries-and-filters/queries-and-filters-osgi-command/src/main/java/com/liferay/search/samples/osgi/commands/QueriesAndFiltersOSGiCommand.java
+++ dev/queries-and-filters/queries-and-filters-osgi-command/src/main/java/com/liferay/search/samples/osgi/commands/QueriesAndFiltersOSGiCommand.java
@@ -5,21 +5,28 @@
 
 package com.liferay.search.samples.osgi.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.search.document.Document;
+import com.liferay.portal.search.hits.SearchHit;
+import com.liferay.portal.search.hits.SearchHits;
 import com.liferay.portal.search.query.BooleanQuery;
 import com.liferay.portal.search.query.MatchQuery;
 import com.liferay.portal.search.query.Queries;
-import com.liferay.portal.search.query.TermsQuery;
+import com.liferay.portal.search.query.TermQuery;
 import com.liferay.portal.search.searcher.SearchRequest;
 import com.liferay.portal.search.searcher.SearchRequestBuilder;
 import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.searcher.SearchResponse;
 import com.liferay.portal.search.searcher.Searcher;
-
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 @Component(
 	property = {"osgi.command.function=search", "osgi.command.scope=liferay"},
@@ -31,9 +38,7 @@ public class QueriesAndFiltersOSGiCommand {
 		MatchQuery titleQuery = _queries.match(
 			Field.getLocalizedName(LocaleUtil.US, Field.TITLE), keywords);
 
-		TermsQuery rootFolderQuery = _queries.terms(Field.FOLDER_ID);
-
-		rootFolderQuery.addValues("0");
+		TermQuery rootFolderQuery = _queries.term(Field.FOLDER_ID, "0");
 
 		BooleanQuery booleanQuery = _queries.booleanQuery();
 
@@ -61,10 +66,35 @@ public class QueriesAndFiltersOSGiCommand {
 
 		SearchResponse searchResponse = _searcher.search(searchRequest);
 
+		SearchHits searchHits = searchResponse.getSearchHits();
+
+		List<SearchHit> searchHitsList = searchHits.getSearchHits();
+
+		List<String> resultsList = new ArrayList<>(searchHitsList.size());
+		
+		if (!searchHitsList.isEmpty()) {
+
+		searchHitsList.forEach(
+			searchHit -> {
+				float hitScore = searchHit.getScore();
+
+				Document doc = searchHit.getDocument();
+
+				String uid = doc.getString(Field.UID);
+
+				System.out.println(
+					StringBundler.concat(
+						"Document ", uid, " had a score of ", hitScore));
+
+				resultsList.add(uid);
+			});
+		}
+		else {
 		System.out.println(
 			"Request String:\n" + searchResponse.getRequestString());
 		System.out.println(
 			"Response String:\n" + searchResponse.getResponseString());
+		}
 	}
 
 	@Reference
